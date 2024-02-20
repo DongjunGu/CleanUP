4132f10a8907b6bd556e66e9f7bfc2cd56d2fed2
DongjunGu <Dongjun_gu@student.uml.edu>
2024-02-20 16:16:17 +0900

    Enemy Chase

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -5,17 +5,23 @@ using UnityEngine.AI;
 
 public class Enemy : MonoBehaviour
 {
+
+    [SerializeField] float _scanRange = 10;
+
     public int maxHP;
     public int currentHp;
     public Transform target;
     private bool canTakeDamage = true;
 
+    bool _isDetected;
     Rigidbody rigid;
     BoxCollider boxCol;
     Material mat;
     NavMeshAgent nav;
+    Vector3 enemyOrgPlace;
     void Awake()
     {
+        enemyOrgPlace = transform.position;
         rigid = GetComponent<Rigidbody>();
         boxCol = GetComponent<BoxCollider>();
         mat = GetComponentInChildren<SkinnedMeshRenderer>().material;
@@ -24,9 +30,31 @@ public class Enemy : MonoBehaviour
 
     void Update()
     {
-        nav.SetDestination(target.position);
+        DetectPlayer();
     }
 
+    void DetectPlayer()
+    {
+        float distance = (target.transform.position - transform.position).magnitude;
+        
+        if (distance <= _scanRange) //Detect
+        {
+            _isDetected = true;
+            nav.SetDestination(target.position);
+        }
+
+        if (_isDetected && (distance < 30.0f)) //Chase
+        {
+            nav.SetDestination(target.position);
+        }
+
+        if (distance > 30.0f) //Go Back
+        {
+            //transform.position = enemyOrgPlace;
+            nav.SetDestination(enemyOrgPlace);
+            //NavMesh.CalculatePath
+        }
+    }
     void OnTriggerEnter(Collider other)
     {
         if(other.tag == "basicAttack" && canTakeDamage)

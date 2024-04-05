27c342c3ba8e301aa6cedf1feca00a604a26511c
DongjunGu <112649273+DongjunGu@users.noreply.github.com>
2024-04-05 19:06:58 +0900

    E pos updated

--- Assets/Scripts/PushCubeUI.cs
+++ Assets/Scripts/PushCubeUI.cs
@@ -5,60 +5,35 @@ using UnityEngine.UI;
 
 public class PushCubeUI : MonoBehaviour
 {
-    public Transform target12;
-    public Transform target3;
-    public Transform target6;
-    public Transform target9;
     public Image Eimage;
+    public Transform remy;
     GameObject prefab;
     GameObject EPrefab;
     bool keyPressed = false;
-    int count = 2;
     void Start()
     {
-        
+
     }
 
     private void Update()
     {
-        
+
     }
     private void OnTriggerStay(Collider other)
     {
         Transform player = transform.Find("player");
         if (other.tag == "Player")
         {
-            if(player == null)
+            if (player == null)
             {
-                if (NewPlayerController.direction == 1)
-                {
-                    Eimage.gameObject.SetActive(true);
-                    Eimage.transform.position = Camera.main.WorldToScreenPoint(target12.position);
-                }
-
-                if (NewPlayerController.direction == 2)
-                {
-                    Eimage.gameObject.SetActive(true);
-                    Eimage.transform.position = Camera.main.WorldToScreenPoint(target3.position);
-                }
-
-                if (NewPlayerController.direction == 3)
-                {
-                    Eimage.gameObject.SetActive(true);
-                    Eimage.transform.position = Camera.main.WorldToScreenPoint(target6.position);
-                }
-
-                if (NewPlayerController.direction == 4)
-                {
-                    Eimage.gameObject.SetActive(true);
-                    Eimage.transform.position = Camera.main.WorldToScreenPoint(target9.position);
-                }
-                keyPressed = false;
+                Eimage.gameObject.SetActive(true);
+                Eimage.transform.position = Camera.main.WorldToScreenPoint(remy.position + Vector3.forward * 3f);
             }
-            else if(player != null)
+            else if (player != null)
             {
                 Eimage.gameObject.SetActive(false);
             }
+
         }
     }
     private void OnTriggerExit(Collider other)
